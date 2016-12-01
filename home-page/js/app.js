57833813660597bb5acbfc6e1a1a84b3f864fa5d
Edison Neo <edineo62@MacEddie.local>
2016-12-01 16:53:38 +0800

    update animations

--- home-page/js/app.js
+++ home-page/js/app.js
@@ -1,4 +1,5 @@
 $(".main-title h1")
 	.blast({ delimiter: "character" , customClass: "weeee" })
 	.css("opacity", 0)
-	.velocity("transition.fadeIn", { stagger: 50 });
+	.velocity("transition.fadeIn", { stagger: 50, delay:100, duration: 4000, easing: 'ease-out' })
+	.velocity({translateY:"-100px"},1000, 'ease-in-out');
